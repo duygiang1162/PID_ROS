d5cb79f028c067663441acb9e108715106e21a76
duygiang <ddhduygiang@gmail.com>
2023-09-14 18:02:19 +0700

    processing move arm robot 2 dof

--- src/control_exercises/exercise_2-3-1.py
+++ src/control_exercises/exercise_2-3-1.py
@@ -2,22 +2,16 @@
 import numpy as np
 from matplotlib import pyplot as plt 
 
-#  inertia matrix
 def D_robot(m1,m2,l1,l2,lc1,lc2,Izz1,Izz2,th):
   D=np.zeros((2,2))
   t1=th[0,0]
   t2=th[1,0]
-  
-  # start
-  D[0,0]=
-  D[0,1]=
-  D[1,0]=
-  D[1,1]=
-  # end
-    
+  D[0,0]=Izz1+Izz2+lc1**2*m1+l1**2*m2+2*np.cos(t2)*l1*lc2*m2+lc2**2*m2
+  D[0,1]=Izz2+np.cos(t2)*l1*lc2*m2+lc2**2*m2
+  D[1,0]=Izz2+np.cos(t2)*l1*lc2*m2+lc2**2*m2
+  D[1,1]=Izz2+lc2**2*m2
   return D
 
-#  coriolis term      
 def C_robot(m1,m2,l1,l2,lc1,lc2,Izz1,Izz2,th,dth):
   C=np.zeros((2,2))
   t1=th[0,0]
@@ -26,22 +20,20 @@ def C_robot(m1,m2,l1,l2,lc1,lc2,Izz1,Izz2,th,dth):
   dt1=dth[0,0]
   dt2=dth[1,0]
   
-  # startC[0,0]=0
-  C[0,1]=0
-  C[1,0]=0
-  C[1,1]=0
-  # endreturn C
+  C[0,0]=-2 * np.sin(t2) * l1 * lc2 * m2 * dt2
+  C[0,1]=-np.sin(t2) * l1 * lc2 * m2 * dt2
+  C[1,0]= np.sin(t2) * l1 * lc2 * m2 * dt1
+  C[1,1]= 0.0
 
   return C
 
-# gravity vector
 def g_robot(m1,m2,l1,l2,lc1,lc2,Izz1,Izz2,th,grav):
   gravity=np.zeros((2,1))
   
   t1=th[0,0]
   t2=th[1,0]
-  
-  # startgravity[0,0]=
-  gravity[1,0]=
-  # end
-	return gravity
+
+  gravity[0,0]=0.0
+  gravity[1,0]=0.0
+
+  return gravity
